85bcab355f7598759c8696deb33c279a051f3e5b
Nawaf Kamal <xp.mo91@gmail.com>
2025-07-22 22:27:58 +0300

    finish contact

--- Mungez/main/urls.py
+++ Mungez/main/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 
+app_name = "main"
+
 urlpatterns = [
-    path('', views.main_page),
+    path('', views.main_page, name="home_page"),
+    path('contact/', views.contact_page, name="contact_page"),
+    path('services/', views.services_page, name="services_page"),
+    path('about/', views.about_page, name="about_page"),
 ]
--- Mungez/main/views.py
+++ Mungez/main/views.py
@@ -5,3 +5,15 @@ from django.http import HttpRequest,HttpResponse
 def main_page(request:HttpRequest):
 
   return render(request, "main/index.html")
+
+def contact_page(request:HttpRequest):
+
+  return render(request, "main/contact.html")
+
+def services_page(request:HttpRequest):
+
+  return render(request, "main/services.html")
+
+def about_page(request:HttpRequest):
+
+  return render(request, "main/about.html")
