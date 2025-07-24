6ff0486da23a49a7bdeae38dd95dda2100266126
Nawaf Kamal <xp.mo91@gmail.com>
2025-07-24 20:31:30 +0300

    fix some pages and complete it + footer

--- Mungez/services/models.py
+++ Mungez/services/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Workers(models.Model):
+    name = models.CharField(max_length=30)
+    about = models.TextField(max_length=1024)
+    category = models.CharField(max_length=20)
+    rating = models.DecimalField(max_digits=1, decimal_places=0)
+    
